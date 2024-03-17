5ade4b207cf97fc0f108e57959f9f873d36ccc19
Lisa Nguyen <lisatramnguyen@yahoo.ca>
2024-03-17 12:31:15 -0400

    Created base login page

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 import { Link } from 'expo-router';
 
-export default function Page() {
+export default function Index() {
   return (
-    <View>
+    <View className='items-center'>
         <Text>Temp Page - Navigation</Text>
         <Text><Link href="/login">Login</Link></Text>
     </View>
--- frontend/app/login.js
+++ frontend/app/login.js
@@ -1,10 +1,23 @@
 import { Text, View, TextInput } from "react-native";
+//import { styled } from 'nativewind';
+//const StyledText = styled(Text)
 //import styles from './styles'
 
 export default function Page() {
   return (
-    <View classname=''>
-      <Text>meow</Text>
+    <View className='items-center'>
+      <Text className='text-xl'>Log Into Dwellow</Text>
+      <Text className='font-semibold'>Email Address</Text>
+      <TextInput placeholder="Enter Email"/>
+      <Text className='font-semibold'>Password</Text>
+      <TextInput
+            secureTextEntry
+            placeholder='Enter Password'
+            className=''
+          />
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Login
+      </button>
     </View>
   );
 }
