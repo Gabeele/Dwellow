5000a7558632882217e2a7cccb4de934a4e1fe6f
Lisa Nguyen <lisatramnguyen@yahoo.ca>
2024-03-17 13:15:07 -0400

    Added state placeholders

---
+++ frontend/app/home.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View className='items-center'>
+        <Text className=''>Home</Text>
+    </View>
+  );
+}
--- frontend/app/login.js
+++ frontend/app/login.js
@@ -1,23 +1,44 @@
-import { Text, View, TextInput } from "react-native";
-//import { styled } from 'nativewind';
-//const StyledText = styled(Text)
-//import styles from './styles'
+import { Text, View, TextInput, Button } from "react-native";
+import { Link, router } from 'expo-router';
+import React, { useState } from 'react'
 
-export default function Page() {
+export default function Login() {
+  const [email, setEmail] = useState({ value: ''})
+  const [password, setPassword] = useState({ value: ''})
+
+  const onLoginPressed = () => {
+    // validation
+    // enter form
+  }
+
   return (
     <View className='items-center'>
       <Text className='text-xl'>Log Into Dwellow</Text>
       <Text className='font-semibold'>Email Address</Text>
-      <TextInput placeholder="Enter Email"/>
+      <TextInput 
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text})}
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        className='text-opacity-50 border'
+        placeholder="Enter Email"
+        />
       <Text className='font-semibold'>Password</Text>
-      <TextInput
-            secureTextEntry
-            placeholder='Enter Password'
-            className=''
-          />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Login
-      </button>
+      <TextInput 
+        value={password.value}
+        onChangeText={(text) => setPassword({ value: text})} 
+        secureTextEntry 
+        placeholder='Enter Password'
+        className=''
+      />
+      <Button 
+        title="Login"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onPress={() => {
+          //onLoginPressed
+          router.navigate('/home');
+        }}>
+      </Button>
     </View>
   );
 }
