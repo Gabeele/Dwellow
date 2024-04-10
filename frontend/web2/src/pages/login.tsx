d88a5f5fc9fb25a8cba6abc37a36c4d6e157c988
Gavin Abeele <gabeele2160@conestogac.on.ca>
2024-04-10 17:46:04 -0400

    Login functionality

--- frontend/web2/src/pages/login.tsx
+++ frontend/web2/src/pages/login.tsx
@@ -1,24 +1,44 @@
 import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/Firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Perform login logic here
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Login successful");
+      navigate("/home");
+    } catch (error: any) {
+      console.error("Login error:", error.message);
+      if (
+        error.message == "Firebase: Error (auth/user-not-found)." ||
+        error.message == "Firebase: Error (auth/wrong-password)."
+      ) {
+        setErrorMessage("Invalid email or password");
+      }
+    }
   };
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="bg-white p-8 rounded shadow">
         <h1 className="text-2xl font-bold mb-4">Login to Dwellow</h1>
+        {errorMessage && (
+          <div className="mb-4 text-center text-red-500">{errorMessage}</div>
+        )}
         <Input
           type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <div className="h-4" />
 
