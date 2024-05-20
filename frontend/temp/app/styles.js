65a71dce12b06465ce07136743373d02e0eda4b7
Gavin Abeele <abeele.s.gavin@gmail.com>
2024-05-20 12:21:20 -0400

    Delete frontend/temp directory

--- frontend/temp/app/styles.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    MainWhite: '#F5F5F5',
-    PrimaryGreen: '#368156',
-    SecondaryGreen: '#8FC5A6',
-    TertiaryGreen: '#D3EDDE',
-    MainDark: '#25272D',
-    MainGrey:'#5B606D',
-  }
