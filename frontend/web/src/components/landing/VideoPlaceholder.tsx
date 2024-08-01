430b012283291958a5c36534f4a3a754f1559e05
Gavin Abeele <abeele.s.gavin@gmail.com>
2024-08-01 12:50:42 -0400

    Updating

--- frontend/web/src/components/landing/VideoPlaceholder.tsx
+++ frontend/web/src/components/landing/VideoPlaceholder.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const VideoPlaceholder: React.FC = () => (
-  <div className="w-2/4 h-96 flex items-center justify-center rounded-lg shadow-lg relative p-1 bg-gradient-to-r from-green-600 to-dwellow-dark-200">
-    <div className="bg-white rounded-lg p-6 shadow-md w-full h-full flex items-center justify-center">
-      <p>Video Demo Placeholder</p>
+const VideoPlayer: React.FC = () => (
+  <div className=" max-w-1/2 flex items-center justify-center rounded-lg shadow-lg relative p-1 bg-gradient-to-r from-green-600 to-dwellow-dark-200">
+    <div className="bg-white rounded-lg shadow-md w-full h-full flex items-center justify-center">
+      <video className="w-full h-full" controls src="/Dwellow Features.mp4">
+        Your browser does not support the video tag.
+      </video>
     </div>
   </div>
 );
 
-export default VideoPlaceholder;
+export default VideoPlayer;
